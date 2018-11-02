8272f1570c17080a887e7ef5bf3c3d437c9d07a2
achilleas.loumis P. Loumis <achilleas.loumis@gmail.com>
2018-11-02 17:30:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 39
-var message = `"joe is ${39} inches tall`
+var message = `"joe is ${40} inches tall`
 
 module.exports = {name, height, message}
